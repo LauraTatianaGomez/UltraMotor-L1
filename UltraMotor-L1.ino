00dd1234000179728abfc060e345535313ae3db6
yeikool <yeikolballesteros@gmail.com>
2019-02-19 21:06:33 -0500

    variables

--- UltraMotor-L1.ino
+++ UltraMotor-L1.ino
@@ -1,3 +1,10 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+Servo servoizq; //se define servo izquierdo
+Servo servoder; //se define servo derecho
+SoftwareSerial BT(3,2);//tx rx
+char orden;
+
 void setup() {
   // put your setup code here, to run once:
 
